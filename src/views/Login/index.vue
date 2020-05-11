01e336257e4c0a62e41d9530f0e16ad730f05519
FBI <dacaojia@qq.com>
2020-05-11 17:42:53 +0800

    构建项目、配置全局样式、路由、element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="login">
+		这里需要元素包裹
+		<el-button>
+			按钮
+		</el-button>
+	</div>
+</template>
+<script>
+	
+</script>
+<style lang="scss" scoped="scoped">
+	#login{
+		//vh基于可视区50为可视屏幕一半
+		height: 100vh;
+		background-color: #344a5f;
+	}
+</style>
